bd5f07894ba4f561296cbf26a6812442ea3cb72e
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-09 17:02:07 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('on-check').checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is On</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is Off</p>"
+  }
+}
